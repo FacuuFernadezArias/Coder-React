91b2037ac8bc4e208db14d0fffe33fc1d175db0e
Facundo Fernandez Arias <facuferaria@gmail.com>
2023-09-11 21:46:10 -0300

    Preentrega React

---
+++ src/components/Cart.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {BsCart} from "react-icons/bs"
+
+const Cart = (  ) => { 
+  return ( 
+    <div> <BsCart/> 
+    <span>0</span>
+    </div>
+  );
+};
+
+export default Cart;
